4b5e3cb380267fd1e8153153c9263364711b928b
Tim Savage <tim@savage.company>
2019-05-31 10:55:04 +1000

    Update __init__.py

--- pae/smtp/__init__.py
+++ pae/smtp/__init__.py
@@ -1,5 +1,5 @@
 """
-PyApp SMTP Extension
+pyApp SMTP Extension
 ~~~~~~~~~~~~~~~~~~~~
 
 """
@@ -7,7 +7,7 @@ from pyapp.versioning import get_installed_version
 
 from .factory import *
 
-__name__ = "PyApp SMTP Extension"
+__name__ = "SMTP Extension"
 __version__ = get_installed_version("pyApp-SMTP", __file__)
 __default_settings__ = ".default_settings"
 __checks__ = ".checks"
