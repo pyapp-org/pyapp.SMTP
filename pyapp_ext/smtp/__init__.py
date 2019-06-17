c4af414deef274c83e3e45222ce8c03d5798257b
Tim Savage <tim@savage.company>
2019-06-17 23:43:56 +1000

    Updated to the new style extension registration

---
+++ pyapp_ext/smtp/__init__.py
@@ -0,0 +1,34 @@
+"""
+pyApp SMTP Extension
+~~~~~~~~~~~~~~~~~~~~
+
+"""
+from pyapp.app import CommandGroup, argument
+
+from .client import *
+from .factory import *
+
+
+class Extension:
+    """
+    SMTP Extension
+    """
+    default_settings = ".default_settings"
+    checks = ".checks"
+
+    @staticmethod
+    def register_command(root: CommandGroup):
+        group = root.create_command_group("smtp")
+
+        @group.command
+        @argument("TO", help_text="Message recipient")
+        @argument("SUBJECT", help_text="Message subject")
+        @argument("--from", dest="from_", help_text="Message sender")
+        @argument("--body", help_text="Message body; defaults to 'Test Email'")
+        @argument("--config", default="default", help_text="Email configuration to use.")
+        def send(opts):
+            """
+            Send a test email
+            """
+            smtp_client = create_client(opts.config)
+            smtp_client.send_plain(opts.TO, opts.SUBJECT, opts.body or "Test Email", from_=opts.from_)
